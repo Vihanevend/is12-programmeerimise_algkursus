38aad73c0b6d67f822f6f6c5e1a5823d9c5a1e61
progre <progre@progre-pc.(none)>
2013-09-18 18:14:54 +0300

    valemid 10 tk

---
+++ valemid.py
@@ -0,0 +1,53 @@
+# v=s/t
+S = 55.0
+T = 35.0
+V = S/T
+print V
+
+# h=g*t^2/2 
+G = 10.0
+H = (G*(T*T))/2
+print H
+
+# e=mc^2
+M = 13.0
+C = 5.0
+E = M*C*C
+print E
+
+# i=u/r
+U = 317.0
+R = 43.0
+I = U/R
+print I
+
+# i=e/r+m
+R = 213.0
+I1 = E/(R+M)
+print I1
+
+# v=a^3
+A = 7.0
+V1 = A*A*A
+print V1
+
+# s=d1*d2/2
+D1 = 32.0
+D2 = 18.0
+S1 = (D1*D2)/2
+print S1
+
+# p=2*(a+b)
+B = 21.0
+P = 2*(A+B)
+print P
+
+# sk=n*a*m/2
+N = 14.0
+SK = (N*A*M)/2
+print SK
+
+# v=abc
+V = A*B*C
+print V
+
