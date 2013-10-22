e934a1c26b6ea94eb87f69c1705507ad93e8d42f
progre <progre@progre-pc.(none)>
2013-10-22 20:11:59 +0300

    kodutöö 23.10.13

---
+++ 199kool.py
@@ -0,0 +1,61 @@
+x = 0 
+while x < 10:
+	print '{0:2d} '.format(x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(10 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(20 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(30 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(40 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(50 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(60 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(70 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(80 +x),
+	x = x + 1
+print ""
+
+x = 0
+while x < 10:
+	print '{0:2d} '.format(90 +x),
+	x = x + 1
+print ""
+
+
