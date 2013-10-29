43fd54691495a258d8d5ac2d9b1374772fc7efcc
Ardo Liivamägi <ardo.liivamagi@gmail.com>
2013-10-29 19:13:26 +0200

    Update 199kool.py

--- 199kool.py
+++ 199kool.py
@@ -1,61 +1,14 @@
-x = 0 
-while x < 10:
-	print '{0:2d} '.format(x),
-	x = x + 1
-print ""
+y=0
+while y<10:
 
-x = 0
-while x < 10:
-	print '{0:2d} '.format(10 +x),
-	x = x + 1
-print ""
+        x= 0
+        while x < 10:
+                print "{0:02}".format(10*y+x),
+                x=x+1
+        print ""
+        
+        y=y+1
 
-x = 0
-while x < 10:
-	print '{0:2d} '.format(20 +x),
-	x = x + 1
-print ""
 
-x = 0
-while x < 10:
-	print '{0:2d} '.format(30 +x),
-	x = x + 1
-print ""
-
-x = 0
-while x < 10:
-	print '{0:2d} '.format(40 +x),
-	x = x + 1
-print ""
-
-x = 0
-while x < 10:
-	print '{0:2d} '.format(50 +x),
-	x = x + 1
-print ""
-
-x = 0
-while x < 10:
-	print '{0:2d} '.format(60 +x),
-	x = x + 1
-print ""
-
-x = 0
-while x < 10:
-	print '{0:2d} '.format(70 +x),
-	x = x + 1
-print ""
-
-x = 0
-while x < 10:
-	print '{0:2d} '.format(80 +x),
-	x = x + 1
-print ""
-
-x = 0
-while x < 10:
-	print '{0:2d} '.format(90 +x),
-	x = x + 1
-print ""
 
 
