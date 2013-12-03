b89ba26d3012f6f941183f1065d9290a735bf8da
progre <progre@progre-pc.(none)>
2013-12-03 18:17:27 +0200

    ###2481$

---
+++ 04-12-13kodutoo.py
@@ -0,0 +1,12 @@
+x = 1
+while x < 6:
+        y = 0
+        while y < x:
+                print "$",
+                y = y + 1                        
+        y = 6
+        while x < y:
+                print "#",
+                y = y - 1
+        print
+        x = x + 1
