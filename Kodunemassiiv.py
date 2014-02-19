98f1897f16257a2c57d9673610da36c34e7dc0be
progre <progre@progre-pc.(none)>
2014-02-19 07:37:48 +0200

    kodune töö 19.02.2014

---
+++ Kodunemassiiv.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+filmid = [
+['Bad Grandpa', 'huumor', '2013'],
+['Forgetting Sarah Marshall', 'huumor', '2008'],
+['Get Him to the Creek', 'huumor', '2010'],
+['Titanic', 'kurb', '1992'],
+['Avatar', 'ulme', '2011']
+]
+
+def lisa():
+	nimi = raw_input("Pealkiri: ")
+	zanr = raw_input("Zanr: ")
+	aasta = raw_input("Aasta: ")
+	
+	filmid.append([nimi, zanr, aasta])
+	
+def eemalda():
+	number = int(raw_input("Filmi number: "))	
+	
+	del filmid[number]
+
+print "Algne andmebaas:"
+		
+def naita():
+	for element in filmid:
+		print element
+naita()	
+
+print
+
+while True:
+	valik = raw_input("Tee valik: " + "add - lisa | " + "rm - eemalda | " + " ls - näita listi | " + "või q - lõpeta. ")
+	
+	if valik == 'add':
+		lisa()
+	if valik == 'ls':
+		naita()
+	if valik == 'rm':
+		eemalda()
+	if valik == 'q':
+		break
